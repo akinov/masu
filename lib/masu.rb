96978ccc0dac571b90b4299a310906a104e97aa5
akinov <ayumoto10@gmail.com>
2019-09-15 15:23:31 +0900

    Add type totsuzen!!!

--- lib/masu.rb
+++ lib/masu.rb
@@ -3,11 +3,31 @@ require 'masu/version'
 module Masu
   class Error < StandardError; end
 
-  def to_masu
+  def to_masu(type = :default)
+    method_name = "to_masu_#{type}"
+
+    unless self.class.private_method_defined? method_name
+      raise ArgumentError.new, "Undefined Masu type #{type}."
+    end
+
+    send method_name
+  end
+
+  private
+
+  def to_masu_default
     <<~EOS
       ┌#{'-' * length}┐
       │#{self}│
       └#{'-' * length}┘
+    EOS
+  end
+
+  def to_masu_totsuzen
+    <<~EOS
+      ＿#{'人' * length}＿
+      ＞ #{self} ＜
+      ￣#{('Y^' * length).delete_suffix('^')}￣
     EOS
   end
 end
--- spec/masu_spec.rb
+++ spec/masu_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe Masu do
   it "Enclose text in a frame" do
     expect('hote'.to_masu).to eq("┌----┐\n│hote│\n└----┘\n")
   end
+
+  it "Undefined type call" do
+    expect { 'hote'.to_masu(:hoge) }.to raise_error(ArgumentError)
+  end
 end

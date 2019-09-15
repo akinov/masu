995bcf2fda9ee969513062dbdb2fc6f81d17592e
akinov <ayumoto10@gmail.com>
2019-09-15 13:59:45 +0900

    Imprement to_masu

--- lib/masu.rb
+++ lib/masu.rb
@@ -1,6 +1,17 @@
-require "masu/version"
+require 'masu/version'
 
 module Masu
   class Error < StandardError; end
-  # Your code goes here...
+
+  def to_masu
+    <<~EOS
+      ┌#{'-' * length}┐
+      │#{self}│
+      └#{'-' * length}┘
+    EOS
+  end
+end
+
+class String
+  prepend Masu
 end
